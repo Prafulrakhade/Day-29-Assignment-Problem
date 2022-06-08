4cb1ffd7bdf70a0a2591a202b946c4880b232e7a
Praful <prafulrakhade02@gmail.com>
2022-06-08 15:31:36 +0530

    [praful] Added Indian Census Analyser Program

---
+++ IndianStateCensusAnalyser/CensusAnalyserException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser.POCO
+{
+    /// <summary>
+    /// Custom Exception class
+    /// </summary>
+    public class CensusAnalyserException : Exception
+    {
+        /// <summary>
+        /// Enum for Custom exception type 
+        /// </summary>
+        public enum ExceptionType
+        {
+            FILE_NOT_FOUND, INVALID_FILE_TYPE, INCORRECT_DELIMITER, INCORRECT_HEADER, NO_SUCH_COUNTRY
+        }
+        public ExceptionType eType;
+        public CensusAnalyserException(string message, ExceptionType exceptionType) : base(message)
+        {
+            this.eType = exceptionType;
+        }
+    }
+}
